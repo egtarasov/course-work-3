e863eab671b9b6eedee77b9a28745c551ec039cc
egtarasov <taraeg2004@gmail.com>
2025-01-31 01:02:06 +0300

    init

---
+++ create-cluster.sh
@@ -0,0 +1,15 @@
+service_account=?
+subnets=?
+admin_password=?
+
+yc managed-airflow cluster create \
+   --name course-work \
+   --admin-password $admin_password \
+   --service-account-id $service_account \
+   --subnet-ids $subnets \
+   --webserver count=1,resource-preset-id=c1-m2 \
+   --scheduler count=1,resource-preset-id=c1-m2 \
+   --worker count=1,resource-preset-id=c1-m2 \
+   --triggerer count=1,resource-preset-id=c1-m2 \
+   --dags-bucket course-work-bucket
+
---
+++ dag.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+def fibonachi(n):
+    if n <= 1:
+        return 1
+    return fibonachi(n - 1) + fibonachi(n - 2)
+
+
+def task() -> int:
+    return fibonachi(45)
+
+
+def the_end() -> str:
+    return "The end!"
+
+
+default_args = {'retries': 3}
+
+with DAG(
+    dag_id="dag-with-many-tasks",
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=datetime.today(),
+    tags=['huge-dag'],
+) as dag:
+    for i in range(1, 31):
+        PythonOperator(task_id=f"task-{i}", python_callable=task)
+
+    list(dag.tasks) >> PythonOperator(
+        task_id="the_end",
+        python_callable=the_end,
+    )
+
+
